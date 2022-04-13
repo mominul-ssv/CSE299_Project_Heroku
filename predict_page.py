66b6b9ef870f4ea86f36ead47a300a441514e6af
Mominul Islam <70751323+Levarenox@users.noreply.github.com>
2022-04-13 10:58:00 +0600

    change in prediction parameters

--- predict_page.py
+++ predict_page.py
@@ -17,23 +17,13 @@ regressor_loaded = data["model"]
 def show_predict_page():
     st.title("Depression Prediction")
 
-    st.write("""### Please answer the questions below:""")
+    st.write("""### Please answer the general questions below:""")
 
-    Residence = st.slider(
-        "Residence (1 = Tokyo, 2 = Kanagawa, 3 = Saitama, 4 = Chiba, 5 = Osaka, 6 = Hyogo, 7 = Fukuoka)", 1, 7, 1)
+    st.markdown("""---""")
 
     Sex = st.slider("Sex (1 = Male, 2 = Female)", 1, 2, 1)
 
     Age = st.slider("Age", 0, 100, 18)
-
-    Age_Group = st.slider(
-        "Age Group (Pick '4' for 18-19 years, '3' for 20-39 years, '2' for 40-64 years, '1' for greater than 65 years)", 1, 4, 1)
-
-    Job_Group = st.slider(
-        "Employment Status (1 = Employed, 2 = Home Maker, 3 = Student, 4 = Unemployed, 5 = Others)", 1, 5, 1)
-
-    Health_Worker_Self = st.slider(
-        "Are you a healthcare worker? (1 = Yes, 2 = No)", 1, 2, 1)
 
     Health_Worker_Family = st.slider(
         "Is your family member a healthcare worker? (1 = Yes, 2 = No)", 1, 2, 1)
@@ -42,13 +32,7 @@ def show_predict_page():
         "Are you married? (1 = Yes, 2 = No)", 1, 2, 1)
 
     Income = st.slider(
-        "What is your annual household income (JPY)?  (1 = less than 2 million, 2 = 2-4 million, 3 = 4-6 million, 4 = 6-8 million, 5 = 8-10 million, 6 = 10-12 million, 7 = 12-15 million, 8 = 15-20 million, 9 = ≥20 million, 10 = unknown", 1, 10, 1)
-
-    Current_Physical = st.slider(
-        "Are you currently under any treatment for severe physical illness? (1 = Yes, 2 = No)", 1, 2, 1)
-
-    Past_Physical = st.slider(
-        "Have you done gone through any past treatment for severe physical illness? (1 = Yes, 2 = No)", 1, 2, 1)
+        "What is your annual household income (JPY)?  (1 = less than 2 million, 2 = 2-4 million, 3 = 4-6 million, 4 = 6-8 million, 5 = 8-10 million, 6 = 10-12 million, 7 = 12-15 million, 8 = 15-20 million, 9 = ≥20 million, 10 = unknown", 1, 10, 2)
 
     Current_Mental = st.slider(
         "Current treatment for mental problem (1 = Yes, 2 = No", 1, 2, 1)
@@ -56,82 +40,142 @@ def show_predict_page():
     Past_Mental = st.slider(
         "Past treatment for mental problem (1 = Yes, 2 = No)", 1, 2, 1)
 
-    K6 = 10
-    PHQ9 = 6
-    UCLA_LS3 = 25
-    LSNS6 = 15
-
     Exercise = st.slider(
-        "I exercised for my health (whether indoors or outdoors) (1 = not at all, 7 = extremely)", 1, 7, 1)
+        "I exercised for my health (whether indoors or outdoors) (1 = not at all, 7 = extremely)", 1, 7, 2)
 
     Healthy_Diet = st.slider(
-        "I took meals considering the nutrition balance (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I took meals considering the nutrition balance (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Healthy_Sleep = st.slider(
-        "I kept regular awakening time and bedtime approximately (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I kept regular awakening time and bedtime approximately (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Activity = st.slider(
-        "I engaged in activities such as hobbies with absorbing interest (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I engaged in activities such as hobbies with absorbing interest (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Interaction_Offline = st.slider(
-        "I interacted with my family or friends on a face-to-face basis (except work or class) (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I interacted with my family or friends on a face-to-face basis (except work or class) (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Interaction_Online = st.slider(
-        "I interacted with my family or friends online using chat or video calling (except work or class) (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I interacted with my family or friends online using chat or video calling (except work or class) (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Preventive_Behaviors = st.slider(
-        "I spontaneously refrained from going out or altruistically took preventive behaviors (e.g. wearing a mask) to prevent COVID-19 infection to my family or other people (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I spontaneously refrained from going out or altruistically took preventive behaviors (e.g. wearing a mask) to prevent COVID-19 infection to my family or other people (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Optimism = st.slider(
-        "I thought about the future positively (1 = Not at all and 7 = Extreme)", 1, 7, 1)
-
-    Deterioration_Economy = st.slider(
-        "The family budget has tightened (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I thought about the future positively (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Deterioration_Interact = st.slider(
-        "A personal relationship with a close person such as family or friends got worse (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "A personal relationship with a close person such as family or friends got worse (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Frustration = st.slider(
-        "I have become easily annoyed or irate due to life-change (1 = Not at all and 7 = Extreme)", 1, 7, 1)
-
-    Covid_Anxiety = st.slider(
-        "I felt nervous or anxious when I watched news about COVID-19 (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I have become easily annoyed or irate due to life-change (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Covid_Sleepless = st.slider(
-        "I could not sleep because I worried about getting COVID-19 (1 = Not at all and 7 = Extreme)", 1, 7, 1)
-
-    Difficulty_Living = st.slider(
-        "My daily life was interrupted due to the shortage of materials relating to prevention for COVID-19 infection (e.g. mask or thermometer) or other daily supplies (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "I could not sleep because I worried about getting COVID-19 (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
     Difficulty_Work = st.slider(
-        "My work or schoolwork was interrupted due to life-change (1 = Not at all and 7 = Extreme)", 1, 7, 1)
+        "My work or schoolwork was interrupted due to life-change (1 = Not at all and 7 = Extreme)", 1, 7, 2)
 
-    # backend calculation
-    # K6 score = [use any input type]
-    # PHQ9 score = [use any input type]
-    # UCLA_LS3 = [use any input type]
+    st.write("""### Please answer the UCLA_LS3 questions below:""")
+
+    st.markdown("""---""")
+
+    # range = (
+    #     "Never",
+    #     "Rarely",
+    #     "Sometimes",
+    #     "Often",
+    # )
+
+    question_1 = st.slider(
+        "[1] How often do you feel that you are 'in tune' with the people around you? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_2 = st.slider(
+        "[2] How often do you feel that you lack companionship? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_3 = st.slider(
+        "[3] How often do you feel that there is no one you can turn to? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_4 = st.slider(
+        "[4] How often do you feel alone? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_5 = st.slider(
+        "[5] How often do you feel part of a group of friends? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_6 = st.slider(
+        "[6] How often do you feel that you have a lot in common with the people around you? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_7 = st.slider(
+        "[7] How often do you feel that you are no longer close to anyone? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_8 = st.slider(
+        "[8] How often do you feel that your interests and ideas are not shared by those around you? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_9 = st.slider(
+        "[9] How often do you feel outgoing and friendly? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_10 = st.slider(
+        "[10] How often do you feel close to people? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_11 = st.slider(
+        "[11] How often do you feel left out? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_12 = st.slider(
+        "[12] How often do you feel that your relationships with others are not meaningful? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_13 = st.slider(
+        "[13] How often do you feel that no one really knows you well? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_14 = st.slider(
+        "[14] How often do you feel isolated from others? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_15 = st.slider(
+        "[15] How often do you fee1 you can find companionship when you want it? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_16 = st.slider(
+        "[16] How often do you feel that there are people who really understand you? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_17 = st.slider(
+        "[17] How often do you feel shy? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_18 = st.slider(
+        "[18] How often do you feel that people are around you but not with you? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_19 = st.slider(
+        "[19] How often do you feel that there are people you can talk to? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    question_20 = st.slider(
+        "[20] How often do you feel that there are people you can turn to? (1=Never, 2=Rarely, 3=Sometimes, and 4=Often)", 1, 4, 2)
+
+    # def range_convert(question):
+    #     if question == "Never":
+    #         question = 1
+    #     elif question == "Rarely":
+    #         question = 2
+    #     elif question == "Sometimes":
+    #         question = 3
+    #     elif question == "Often":
+    #         question = 3
+
+    UCLA_LS3 = question_1 + question_2 + question_3 + \
+        question_4 + question_5 + question_6 + question_7 + \
+        question_8 + question_9 + question_10 + question_11 + \
+        question_12 + question_13 + question_14 + \
+        question_15 + question_16 + question_17 + \
+        question_18 + question_19 + question_20
 
     ok = st.button("Calculate Mental State")
 
     if ok:
         X = np.array([[
-            Residence,
             Sex,
             Age,
-            Age_Group,
-            Job_Group,
-            Health_Worker_Self,
             Health_Worker_Family,
             Married,
             Income,
-            Current_Physical,
-            Past_Physical,
             Current_Mental,
             Past_Mental,
-            K6,
-            PHQ9,
             UCLA_LS3,
-            LSNS6,
             Exercise,
             Healthy_Diet,
             Healthy_Sleep,
@@ -140,14 +184,14 @@ def show_predict_page():
             Interaction_Online,
             Preventive_Behaviors,
             Optimism,
-            Deterioration_Economy,
             Deterioration_Interact,
             Frustration,
-            Covid_Anxiety,
             Covid_Sleepless,
-            Difficulty_Living,
             Difficulty_Work
         ]])
 
         state = regressor_loaded.predict(X)
-        st.subheader(f"Mental State: {state[0]}")
+        if state[0] == 0:
+            st.subheader("Not Depressed.")
+        elif state[0] == 1:
+            st.subheader("Depressed!")
